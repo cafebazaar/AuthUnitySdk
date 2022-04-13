8b0fbf68b380709c68482bcafe80da42dd19b620
Mansour <mansurjavadi@gmail.com>
2022-04-13 15:06:13 +0430

    Update android dependency

--- Assets/Cafebazaar/PluginVersion.cs
+++ Assets/Cafebazaar/PluginVersion.cs
@@ -3,6 +3,6 @@ namespace CafeBazaar
     public class PluginVersion
     {
         public const int VersionInt = 2;
-        public const string VersionString = "1.0.0-alpha02";
+        public const string VersionString = "1.0.0-beta01";
     }
 }
